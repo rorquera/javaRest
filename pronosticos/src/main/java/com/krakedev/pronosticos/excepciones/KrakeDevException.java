2ccc701ff06a201ded6fb14a1a8c7dcdab9c452f
rorquera <arckanhell1@gmail.com>
2024-12-14 21:07:41 -0500

    creacion de proyecto web rest para pronosticos de partidos copa america

---
+++ pronosticos/src/main/java/com/krakedev/pronosticos/excepciones/KrakeDevException.java
@@ -0,0 +1,10 @@
+package com.krakedev.pronosticos.excepciones;
+
+public class KrakeDevException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public KrakeDevException(String mensaje) {
+		super(mensaje);
+	}
+
+}
